8ff3d9b4afe720703de5133bde1edfc6e4754316
imzzy1201 <imzzy1201@local.host>
2025-05-15 16:45:39 +0800

    todo: fix basketball_environment

--- examples/async_sac_state_sim_basketball/basketball_environment.py
+++ examples/async_sac_state_sim_basketball/basketball_environment.py
@@ -17,7 +17,7 @@ from franka_sim.controllers import opspace
 from franka_sim.mujoco_gym_env import GymRenderingSpec, MujocoGymEnv
 
 _HERE = Path(__file__).parent
-_XML_PATH = _HERE / "xmls" / "arena.xml"
+_XML_PATH = _HERE / "franka_basket.xml"
 _PANDA_HOME = np.asarray((0, -0.785, 0, -2.35, 0, 1.57, np.pi / 4))
 _CARTESIAN_BOUNDS = np.asarray([[0.2, -0.3, 0], [0.6, 0.3, 0.5]])
 _SAMPLING_BOUNDS = np.asarray([[0.25, -0.25], [0.55, 0.25]])
@@ -28,7 +28,7 @@ class FrankaBasketEnv(MujocoGymEnv):
 
     def __init__(
         self,
-        action_scale: np.ndarray = np.asarray([0.1, 1]),
+        action_scale: float = 0.1,
         seed: int = 0,
         control_dt: float = 0.02,
         physics_dt: float = 0.002,
@@ -36,6 +36,7 @@ class FrankaBasketEnv(MujocoGymEnv):
         render_spec: GymRenderingSpec = GymRenderingSpec(),
         render_mode: Literal["rgb_array", "human"] = "rgb_array",
         image_obs: bool = False,
+        target_pos: np.ndarray = None,
     ):
         self._action_scale = action_scale
 
@@ -48,11 +49,15 @@ class FrankaBasketEnv(MujocoGymEnv):
             render_spec=render_spec,
         )
 
+        # default target at geometric center of workspace
+        self._target_pos = (
+            np.mean(_CARTESIAN_BOUNDS, axis=0)
+            if target_pos is None
+            else target_pos
+        )
+
         self.metadata = {
-            "render_modes": [
-                "human",
-                "rgb_array",
-            ],
+            "render_modes": ["human", "rgb_array"],
             "render_fps": int(np.round(1.0 / self.control_dt)),
         }
 
@@ -60,155 +65,91 @@ class FrankaBasketEnv(MujocoGymEnv):
         self.camera_id = (0, 1)
         self.image_obs = image_obs
 
-        # Caching.
-        self._panda_dof_ids = np.asarray(
-            [self._model.joint(f"joint{i}").id for i in range(1, 8)]
-        )
-        self._panda_ctrl_ids = np.asarray(
-            [self._model.actuator(f"actuator{i}").id for i in range(1, 8)]
-        )
-        self._gripper_ctrl_id = self._model.actuator("fingers_actuator").id
-        self._pinch_site_id = self._model.site("pinch").id
+        # Caching
+        self._panda_dof_ids = np.asarray([
+            self._model.joint(f"joint{i}").id for i in range(1, 8)
+        ])
+        self._panda_ctrl_ids = np.asarray([
+            self._model.actuator(f"actuator{i}").id for i in range(1, 8)
+        ])
+        self._block_geom_id = self._model.geom("block").id
         self._block_z = self._model.geom("block").size[2]
 
-        self.observation_space = gym.spaces.Dict(
-            {
-                "state": gym.spaces.Dict(
-                    {
-                        "panda/tcp_pos": spaces.Box(
-                            -np.inf, np.inf, shape=(3,), dtype=np.float32
-                        ),
-                        "panda/tcp_vel": spaces.Box(
-                            -np.inf, np.inf, shape=(3,), dtype=np.float32
-                        ),
-                        "panda/gripper_pos": spaces.Box(
-                            -np.inf, np.inf, shape=(1,), dtype=np.float32
-                        ),
-                        # "panda/joint_pos": spaces.Box(-np.inf, np.inf, shape=(7,), dtype=np.float32),
-                        # "panda/joint_vel": spaces.Box(-np.inf, np.inf, shape=(7,), dtype=np.float32),
-                        # "panda/joint_torque": specs.Array(shape=(21,), dtype=np.float32),
-                        # "panda/wrist_force": specs.Array(shape=(3,), dtype=np.float32),
-                        "block_pos": spaces.Box(
-                            -np.inf, np.inf, shape=(3,), dtype=np.float32
-                        ),
-                    }
-                ),
-            }
-        )
-
-        if self.image_obs:
-            self.observation_space = gym.spaces.Dict(
-                {
-                    "state": gym.spaces.Dict(
-                        {
-                            "panda/tcp_pos": spaces.Box(
-                                -np.inf, np.inf, shape=(3,), dtype=np.float32
-                            ),
-                            "panda/tcp_vel": spaces.Box(
-                                -np.inf, np.inf, shape=(3,), dtype=np.float32
-                            ),
-                            "panda/gripper_pos": spaces.Box(
-                                -np.inf, np.inf, shape=(1,), dtype=np.float32
-                            ),
-                        }
-                    ),
-                    "images": gym.spaces.Dict(
-                        {
-                            "front": gym.spaces.Box(
-                                low=0,
-                                high=255,
-                                shape=(render_spec.height, render_spec.width, 3),
-                                dtype=np.uint8,
-                            ),
-                            "wrist": gym.spaces.Box(
-                                low=0,
-                                high=255,
-                                shape=(render_spec.height, render_spec.width, 3),
-                                dtype=np.uint8,
-                            ),
-                        }
-                    ),
-                }
+        # Observation space
+        state_spaces = {
+            "panda/tcp_pos": spaces.Box(-np.inf, np.inf, shape=(3,), dtype=np.float32),
+            # "panda/tcp_vel": spaces.Box(-np.inf, np.inf, shape=(3,), dtype=np.float32),
+        }
+        if not self.image_obs:
+            state_spaces["block_pos"] = spaces.Box(
+                -np.inf, np.inf, shape=(3,), dtype=np.float32
             )
 
-        self.action_space = gym.spaces.Box(
-            low=np.asarray([-1.0, -1.0, -1.0, -1.0]),
-            high=np.asarray([1.0, 1.0, 1.0, 1.0]),
-            dtype=np.float32,
+        obs_dict = {"state": spaces.Dict(state_spaces)}
+        if self.image_obs:
+            obs_dict["images"] = spaces.Dict({
+                "front": spaces.Box(
+                    low=0, high=255,
+                    shape=(render_spec.height, render_spec.width, 3),
+                    dtype=np.uint8,
+                ),
+                "wrist": spaces.Box(
+                    low=0, high=255,
+                    shape=(render_spec.height, render_spec.width, 3),
+                    dtype=np.uint8,
+                ),
+            })
+
+        self.observation_space = spaces.Dict(obs_dict)
+
+        # Action space: x, y, z increments
+        self.action_space = spaces.Box(
+            low=-1.0, high=1.0, shape=(3,), dtype=np.float32
         )
 
-        # NOTE: gymnasium is used here since MujocoRenderer is not available in gym. It
-        # is possible to add a similar viewer feature with gym, but that can be a future TODO
         from gymnasium.envs.mujoco.mujoco_rendering import MujocoRenderer
-
-        self._viewer = MujocoRenderer(
-            self.model,
-            self.data,
-        )
+        self._viewer = MujocoRenderer(self.model, self.data)
         self._viewer.render(self.render_mode)
 
-    def reset(
-        self, seed=None, **kwargs
-    ) -> Tuple[Dict[str, np.ndarray], Dict[str, Any]]:
-        """Reset the environment."""
+    def reset(self, seed=None, **kwargs) -> Tuple[Dict[str, np.ndarray], Dict[str, Any]]:
         mujoco.mj_resetData(self._model, self._data)
 
-        # Reset arm to home position.
+        # Reset arm to home
         self._data.qpos[self._panda_dof_ids] = _PANDA_HOME
         mujoco.mj_forward(self._model, self._data)
 
-        # Reset mocap body to home position.
-        tcp_pos = self._data.sensor("2f85/pinch_pos").data
-        self._data.mocap_pos[0] = tcp_pos
+        # Initialize target
+        self._current_target = self._target_pos.copy()
 
-        # Sample a new block position.
+        # Sample new block position
         block_xy = np.random.uniform(*_SAMPLING_BOUNDS)
         self._data.jnt("block").qpos[:3] = (*block_xy, self._block_z)
         mujoco.mj_forward(self._model, self._data)
 
-        # Cache the initial block height.
-        self._z_init = self._data.sensor("block_pos").data[2]
+        # Cache block heights from joint
+        block_qpos = self._data.jnt("block").qpos
+        self._z_init = block_qpos[2]
         self._z_success = self._z_init + 0.2
 
         obs = self._compute_observation()
         return obs, {}
 
-    def step(
-        self, action: np.ndarray
-    ) -> Tuple[Dict[str, np.ndarray], float, bool, bool, Dict[str, Any]]:
-        """
-        take a step in the environment.
-        Params:
-            action: np.ndarray
+    def step(self, action: np.ndarray) -> Tuple[Dict[str, np.ndarray], float, bool, bool, Dict[str, Any]]:
+        dx, dy, dz = action
 
-        Returns:
-            observation: dict[str, np.ndarray],
-            reward: float,
-            done: bool,
-            truncated: bool,
-            info: dict[str, Any]
-        """
-        x, y, z, grasp = action
+        # Update target
+        self._current_target = np.clip(
+            self._current_target + np.asarray([dx, dy, dz]) * self._action_scale,
+            *_CARTESIAN_BOUNDS,
+        )
 
-        # Set the mocap position.
-        pos = self._data.mocap_pos[0].copy()
-        dpos = np.asarray([x, y, z]) * self._action_scale[0]
-        npos = np.clip(pos + dpos, *_CARTESIAN_BOUNDS)
-        self._data.mocap_pos[0] = npos
-
-        # Set gripper grasp.
-        g = self._data.ctrl[self._gripper_ctrl_id] / 255
-        dg = grasp * self._action_scale[1]
-        ng = np.clip(g + dg, 0.0, 1.0)
-        self._data.ctrl[self._gripper_ctrl_id] = ng * 255
-
+        # IK control
         for _ in range(self._n_substeps):
+            # print("xxxx", self._data.mocap_quat)
             tau = opspace(
                 model=self._model,
                 data=self._data,
-                site_id=self._pinch_site_id,
-                dof_ids=self._panda_dof_ids,
-                pos=self._data.mocap_pos[0],
+                pos=self._current_target,
                 ori=self._data.mocap_quat[0],
                 joint=_PANDA_HOME,
                 gravity_comp=True,
@@ -218,80 +159,45 @@ class FrankaBasketEnv(MujocoGymEnv):
 
         obs = self._compute_observation()
         rew = self._compute_reward()
-        terminated = self.time_limit_exceeded()
-
-        return obs, rew, terminated, False, {}
+        done = self.time_limit_exceeded()
+        return obs, rew, done, False, {}
 
     def render(self):
-        rendered_frames = []
-        for cam_id in self.camera_id:
-            rendered_frames.append(
-                self._viewer.render(render_mode="rgb_array", camera_id=cam_id)
-            )
-        return rendered_frames
-
-    # Helper methods.
+        return [
+            self._viewer.render(render_mode="rgb_array", camera_id=i)
+            for i in self.camera_id
+        ]
 
     def _compute_observation(self) -> dict:
-        obs = {}
-        obs["state"] = {}
-
-        tcp_pos = self._data.sensor("2f85/pinch_pos").data
-        obs["state"]["panda/tcp_pos"] = tcp_pos.astype(np.float32)
-
-        tcp_vel = self._data.sensor("2f85/pinch_vel").data
-        obs["state"]["panda/tcp_vel"] = tcp_vel.astype(np.float32)
-
-        gripper_pos = np.array(
-            self._data.ctrl[self._gripper_ctrl_id] / 255, dtype=np.float32
-        )
-        obs["state"]["panda/gripper_pos"] = gripper_pos
-
-        # joint_pos = np.stack(
-        #     [self._data.sensor(f"panda/joint{i}_pos").data for i in range(1, 8)],
-        # ).ravel()
-        # obs["panda/joint_pos"] = joint_pos.astype(np.float32)
-
-        # joint_vel = np.stack(
-        #     [self._data.sensor(f"panda/joint{i}_vel").data for i in range(1, 8)],
-        # ).ravel()
-        # obs["panda/joint_vel"] = joint_vel.astype(np.float32)
-
-        # joint_torque = np.stack(
-        # [self._data.sensor(f"panda/joint{i}_torque").data for i in range(1, 8)],
-        # ).ravel()
-        # obs["panda/joint_torque"] = symlog(joint_torque.astype(np.float32))
-
-        # wrist_force = self._data.sensor("panda/wrist_force").data.astype(np.float32)
-        # obs["panda/wrist_force"] = symlog(wrist_force.astype(np.float32))
-
-        if self.image_obs:
-            obs["images"] = {}
-            obs["images"]["front"], obs["images"]["wrist"] = self.render()
+        obs = {"state": {}}
+        obs_state = obs["state"]
+        obs_state["panda/tcp_pos"] = self._current_target.astype(np.float32)
+        # obs_state["panda/tcp_vel"] = self._data.sensor(
+        #     "2f85/pinch_vel").data.astype(np.float32)
+        if not self.image_obs:
+            # block position from joint qpos
+            obs_state["block_pos"] = self._data.jnt("block").qpos[:3].astype(np.float32)
         else:
-            block_pos = self._data.sensor("block_pos").data.astype(np.float32)
-            obs["state"]["block_pos"] = block_pos
-
+            imgs = self.render()
+            obs["images"] = {"front": imgs[0], "wrist": imgs[1]}
         if self.render_mode == "human":
             self._viewer.render(self.render_mode)
-
         return obs
 
     def _compute_reward(self) -> float:
-        block_pos = self._data.sensor("block_pos").data
-        tcp_pos = self._data.sensor("2f85/pinch_pos").data
+        block_pos = self._data.jnt("block").qpos[:3]
+        tcp_pos = self._current_target
         dist = np.linalg.norm(block_pos - tcp_pos)
         r_close = np.exp(-20 * dist)
         r_lift = (block_pos[2] - self._z_init) / (self._z_success - self._z_init)
         r_lift = np.clip(r_lift, 0.0, 1.0)
-        rew = 0.3 * r_close + 0.7 * r_lift
-        return rew
+        return 0.3 * r_close + 0.7 * r_lift
 
 
 if __name__ == "__main__":
     env = FrankaBasketEnv(render_mode="human")
     env.reset()
-    for i in range(100):
-        env.step(np.random.uniform(-1, 1, 4))
+    for _ in range(100):
+        env.step(np.random.uniform(-1, 1, 3))
         env.render()
     env.close()
