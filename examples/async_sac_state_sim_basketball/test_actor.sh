e1b8020b9c786779749d6778be47ff105530c525
HaoxuHuang <huanghx24@mails.tsinghua.edu.cn>
2025-05-23 14:59:56 +0800

    add test.py

--- examples/async_sac_state_sim_basketball/test_actor.sh
+++ examples/async_sac_state_sim_basketball/test_actor.sh
@@ -1,11 +1,12 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.05 && \
-python async_sac_state_sim.py "$@" \
+python test.py "$@" \
     --actor \
     --render \
     --exp_name=serl_dev_sim_test \
     --seed 0 \
     --port 1145 \
     --random_steps 10 \
-    --actor_steps 40000
+    --actor_steps 40000 \
+    --load_checkpoint "/home/drl/Code/serl/examples/async_sac_state_sim_basketball/checkpoints/checkpoints_10-14-27/checkpoint_560000" \
     # --debug
